05c7f74b7bb274093b40b25fb415891554449470
Noah Fajarda <noahfajarda@gmail.com>
2023-12-09 20:09:50 -0800

    added navbar

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <div>
+      {/* <h1 className="text-6xl">Enter The Net VR</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        culpa non id voluptatem incidunt magnam numquam minus est eius
+        voluptatibus omnis, similique aliquam ex ut officiis veritatis ducimus!
+        Provident, molestias.
+      </p>
+      <div>
+        <button>button text</button>
+        <div>other text</div>
+      </div> */}
+    </div>
+  );
+}
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // is mobile use effect
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 960);
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // handle menu click
+  const handleMenuClick = () => {
+    const navEl = document.querySelector(".navbar-demo");
+    const spanEls = document.querySelectorAll("#pencet span");
+    const menuEl = document.querySelector("#menu");
+    if (menuEl == null || navEl == null || spanEls == null) return;
+    if (isOpen) {
+      setIsOpen(false);
+      navEl.classList.remove("open-nav");
+      spanEls.forEach((el) => el.classList.remove("change-color"));
+      menuEl.classList.remove("Diam");
+    } else {
+      navEl.classList.add("open-nav");
+      spanEls.forEach((el) => el.classList.add("change-color"));
+      menuEl.classList.add("Diam");
+      setIsOpen(true);
+    }
+  };
+
+  return (
+    <header className="w-full flex justify-between navbar-demo">
+      {isMobile && (
+        <div id="menu-icon-container">
+          <div id="menu" onClick={handleMenuClick}>
+            <div id="pencet">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="nav-bar">
+        <ul>
+          <li>
+            <a href="#">Contact</a>
+            <a href="#">Influencer Program</a>
+            <a href="#">Net VR Theater</a>
+            <a href="#">News</a>
+            <a href="#">The Team</a>
+            <a href="#">Creators</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
