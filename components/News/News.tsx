0e50f87411a69cf78da12671772867f86835a471
Luqman <luqmannablan@gmail.com>
2023-12-20 14:18:26 -0600

    added id attribute to news

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -6,9 +6,9 @@ export default function News() {
 
   // Proably can make this into prop so can call it from somewhere else
   const news = [
-    { nameCard: 'Disrupt Magazine (Tech + Startups)', image: 'http://3.143.19.230/assets/Disrupt_Magazine-b706d534.png', date: 'May 16, 2022', body: 'DKyle Doran and The Net VR Creates a Unique and Innovative Experience for Livestreamers and Viewers' },
-    { nameCard: 'Free Apps For Me', image: 'http://3.143.19.230/assets/Home-Theater1-1698e2af.png', date: 'April 27, 2022', body: 'The Net VR Theater Game Review' },
-    { nameCard: 'A Gaming Moment (podcast)', image: 'http://3.143.19.230/assets/Home-Theater2-2a0a8d71.png', date: 'February 11, 2022', body: 'S3E4: What is VR With Kyle Doran CEO of The Net VR' },
+    { id: 1, nameCard: 'Disrupt Magazine (Tech + Startups)', image: 'http://3.143.19.230/assets/Disrupt_Magazine-b706d534.png', date: 'May 16, 2022', body: 'DKyle Doran and The Net VR Creates a Unique and Innovative Experience for Livestreamers and Viewers' },
+    { id: 2, nameCard: 'Free Apps For Me', image: 'http://3.143.19.230/assets/Home-Theater1-1698e2af.png', date: 'April 27, 2022', body: 'The Net VR Theater Game Review' },
+    { id: 3, nameCard: 'A Gaming Moment (podcast)', image: 'http://3.143.19.230/assets/Home-Theater2-2a0a8d71.png', date: 'February 11, 2022', body: 'S3E4: What is VR With Kyle Doran CEO of The Net VR' },
   ]
   return (
     <div className="flex flex-col py-4 md:py-16 gap-6 px-10 lg:px-20 xl:px-72">
@@ -30,7 +30,7 @@ export default function News() {
       <div className="grid xl:grid-cols-3 gap-20 p-5 sm:grid-cols-2 grid-cols-1">
         {news.map((card, idx) => (
           <div key={idx}>
-            <Reveal delay={0.4 + (idx + 1) / 5}>
+            <Reveal delay={0.4 + (card.id) / 5}>
               <div>
                 <div className="flex justify-center">
                   <img
