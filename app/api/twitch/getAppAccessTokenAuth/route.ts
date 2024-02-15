97122a52bae72338e22ddf7ff96cb7059b87addd
Gregory Herringer <greg.herringer@gmail.com>
2024-02-14 18:54:53 -0800

    clean up code, remove unused GET and POST functions, remove unused import, remove console log statements

--- app/api/twitch/getAppAccessTokenAuth/route.ts
+++ app/api/twitch/getAppAccessTokenAuth/route.ts
@@ -1,25 +1,3 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    contact:
-      "this is data from 'authy auth' route. Make a POST Request to send your message",
-  });
-}
-
-export async function POST(req: any) {
-  try {
-    // retrieve email from user request body
-    const TwitchTokenResponse = await req.json();
-    console.log('\nINSIDE POST REQUEST FOR APP ACCESS');
-    console.log(TwitchTokenResponse);
-  } catch (error) {
-
-    console.error(error);
-    
-  }
-}
-
 type TwitchTokenResponse = {
   access_token: string;
   expires_in: number;
@@ -31,13 +9,12 @@ const CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET || '';
 
 export default async function authHandler() {
   try {
-    console.log(CLIENT_SECRET);
-    console.log(CLIENT_ID);
     const response = await fetch('https://id.twitch.tv/oauth2/token', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
+
       // Use URLSearchParams to encode the body with x-www-form-urlencoded format
       body: new URLSearchParams({
         client_id: CLIENT_ID,
@@ -58,7 +35,6 @@ export default async function authHandler() {
 
     console.error(error);
     return;
-    //res.status(500).json({ message: 'Failed to obtain Twitch app access token' });
   }
 }
 
