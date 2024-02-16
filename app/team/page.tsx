27827d52f16c1768103ab34b62787173c02d7337
Noah Fajarda <noahfajarda@gmail.com>
2024-02-16 09:55:11 -0800

    addded window size for responsive design

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "./team.css";
@@ -24,17 +24,24 @@ export default function page() {
     setCurrentIndex(swiper.activeIndex);
   };
 
+  const size = useWindowSize();
+
   return (
     <div>
       <h1>MEET OUR TEAM</h1>
-      <div className="p-5 flex md:flex-row-reverse flex-col-reverse">
+      <div
+        className={`p-5 flex ${
+          size.width > 1000 ? "flex-row-reverse" : "flex-col"
+        }
+      `}
+      >
         <div className="flex flex-col gap-3 w-full h-48">
           <div className="flex flex-col items-center">
             <h2 className="text-3xl">{TeamMembers[currentIndex].name}</h2>
             <h5>{TeamMembers[currentIndex].role}</h5>
             <h5>{TeamMembers[currentIndex].linkedIn}</h5>
           </div>
-          <div className="">{TeamMembers[currentIndex].description}</div>
+          <div>{TeamMembers[currentIndex].description}</div>
         </div>
         <Swiper
           grabCursor={true}
@@ -42,7 +49,9 @@ export default function page() {
           slidesPerView={3}
           // additional
           spaceBetween={10}
-          className="h-96 w-full rounded-lg"
+          className={`h-96 rounded-lg ${
+            size.width > 1000 ? "w-3/5" : "w-full"
+          }`}
           modules={[EffectCoverflow, Pagination]}
           effect="coverflow"
           pagination={true}
@@ -61,4 +70,35 @@ export default function page() {
       </div>
     </div>
   );
+}
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
